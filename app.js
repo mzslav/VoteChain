470f55ecf6fbba5f7044a3679a2ee49f74da7222
mzslav <149380586+mzslav@users.noreply.github.com>
2024-11-06 16:42:57 +0100

    add express server

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import Poll from './models/Poll.js';  
+
+const __dirname = path.resolve();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());  
+app.use(express.static(path.resolve(__dirname, 'static')));  
+
+
+// Підключення до MongoDB
+mongoose.connect('mongodb://localhost:27017/votechain', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.log('MongoDB connection error:', err);
+});
