c1518359e16c9609db26afd2aca546fb35063019
mzslav <149380586+mzslav@users.noreply.github.com>
2024-11-26 19:13:04 +0100

    -voteID(automatical created) +save OptionID (uniq)

--- models/Vote.js
+++ models/Vote.js
@@ -1,14 +1,8 @@
 import mongoose from "mongoose";
 import User from "./User.js"; 
 import Poll from "./Poll.js"; 
+
 const voteSchema = new mongoose.Schema({
-  voteID: {
-    type: Number,
-    unique: true,
-    required: true,
-    index: true,
-    immutable: true
-  },
   userID: {
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'User', 
@@ -22,16 +16,16 @@ const voteSchema = new mongoose.Schema({
     immutable: true,
   },
   chosenOption: {
-    type: [String],
+    type: mongoose.Schema.Types.ObjectId,  // ID вибраної опції
+    ref: 'Poll.options',  // Вказуємо на поле опцій у моделі Poll
     required: true,
-    validate: {
-      validator: function(option) {
-        return option.length > 0;
-      },
-      message: "You must choose at least one option",
-    }
-  }
+  },
+  chosenOptionText: {  // Текст вибраної опції для відображення
+    type: String,
+    required: true,
+  },
 });
+
 
 const Vote = mongoose.model("Vote", voteSchema);
 
